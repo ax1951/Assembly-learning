35ade3bc19441d84de568a226685ac7ccc8911c5
Pete <yingcihtplus@163.com>
2025-01-18 22:47:21 -0500

    Use array length to stop the loop.

--- Chapter3/maximum.s
+++ Chapter3/maximum.s
@@ -11,7 +11,9 @@
 .section .data
 
 data_items:
-    .long 3,67,34,222,45,75,54,34,44,33,22,11,66,0
+    .long 3,67,34,222,45,75,54,34,44,33,22,11,66
+length:
+    .long 13
 
 .section .text
 
@@ -20,9 +22,10 @@ _start:
     movl $0, %edi                   # move 0 into the index register
     movl data_items(,%edi,4), %eax  # load the first byte of data
     movl %eax, %ebx                 # since this is the first item, %eax is the biggest
+    movl length, %ecx
 
 start_loop:
-    cmpl $0, %eax                   # check to see if we've hit the end
+    cmpl %edi, %ecx                   # check to see if we've hit the end
     je loop_exit
     incl %edi                       # load next value
     movl data_items(,%edi,4), %eax
